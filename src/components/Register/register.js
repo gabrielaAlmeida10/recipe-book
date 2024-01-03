ad36b4e8d2f93446175ccd27dcadf40d1eeeb289
Gabriela Almeida <almeida.gabrielafernandes@gmail.com>
2024-01-02 23:54:31 -0300

    feat: register page

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import './register.css';
+import "./register.css";
 
 const Register = () => {
-    return<div>a</div>
-}
+  return (
+    <div className="form">
+      <h1>Cadastro</h1>
+      <form>
+        <label>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="Nome do Usuário"
+          />
+        </label>
+        <label>
+          <span>Email:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Email do Usuário"
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha"
+          />
+        </label>
+        <label>
+          <span>Confirme a Senha:</span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirme sua senha"
+          />
+        </label>
+        <button className="buttton" type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
