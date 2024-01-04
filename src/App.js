31f6d38fbdc96c9fc65dd1fee59386541d8f16c4
Gabriela Almeida <almeida.gabrielafernandes@gmail.com>
2024-01-04 18:23:55 -0300

    feat: adding useContext

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from "react";
 import Routes from "./routes";
 import { BrowserRouter } from "react-router-dom";
 
+import { AuthProvider } from "./context/authContext";
+
 import Menu from "./components/menu/Menu";
 
 import "./App.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Menu />
-      <Routes />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Menu />
+        <Routes />
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 
