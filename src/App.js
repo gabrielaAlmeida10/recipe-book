c21eb6a2b0b01cddbc54a5ae9c037769153caa25
Gabriela Almeida <almeida.gabrielafernandes@gmail.com>
2024-01-05 00:12:09 -0300

    feat: adding authentication logic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Routes from "./routes";
 import { BrowserRouter } from "react-router-dom";
 
 import { AuthProvider } from "./context/authContext";
+import { useAuthentication } from "./hooks/useAuthentication";
+import { onAuthStateChanged } from "firebase/auth";
 
 import Menu from "./components/menu/Menu";
 
 import "./App.css";
 
 const App = () => {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  //assigning the user's loading state the value of the user compared to undefined
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
-    <AuthProvider>
+    <AuthProvider value={{user}}>
       <BrowserRouter>
         <Menu />
         <Routes />
