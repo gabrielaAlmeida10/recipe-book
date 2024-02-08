e2dfd867e686bac9a9a86b6d48fec4fe5afaaa33
Gabriela Almeida <almeida.gabrielafernandes@gmail.com>
2024-02-08 11:42:46 -0300

    feat: add login blocks and orderList to menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuthentication } from "../../hooks/useAuthentication";
+import { useAuthValue } from "../../context/authContext";
 
 import "./Menu.css";
 
 const Menu = () => {
+  const { user } = useAuthValue();
+
   return (
     <nav className="menu">
-      <div className="brand-name"> 
+      <div className="brand-name">
         <Link to="/recipe-book">
           <h1>Recipe Book</h1>
         </Link>
@@ -16,12 +20,19 @@ const Menu = () => {
         <Link to="/OrderList">
           <h3>Order</h3>
         </Link>
-        <Link to="/login">
-          <h3>Login</h3>
+        <Link to="/NewOrder">
+        <h3>New Order</h3>
         </Link>
-        <Link to="/register">
-          <h3>Register</h3>
-        </Link>
+        {!user && (
+          <>
+            <Link to="/login">
+              <h3>Login</h3>
+            </Link>
+            <Link to="/register">
+              <h3>Register</h3>
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
--- src/components/orderList/orderList.js
+++ src/components/orderList/orderList.js
@@ -1,15 +1,19 @@
 import React from "react";
 
 import "./orderList.css";
+import GalleryFilter from "../GalleryView/galleryFilter";
 
 const OrderList = () => {
-    return(
+  return (
     <div className="order-list">
+      <div>
+        <GalleryFilter />
         <div>
-            <h3>Nome pedido</h3>
+          <h3>Nome pedido</h3>
         </div>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default OrderList;
